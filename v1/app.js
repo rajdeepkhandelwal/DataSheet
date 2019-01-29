a817c4a4022796b0f30af9242630157e41fe02fe
khandelwalrajdeep <khandelwalrajdeep@outlook.com>
2019-01-30 00:55:07 +0530

    modify all files

--- v1/app.js
+++ v1/app.js
@@ -3,7 +3,7 @@ var express = require("express"),
     mongoose= require("mongoose"),
     Car    = require("./models/car"),
     bodyParser=require("body-parser");
-mongoose.connect("mongodb://localhost/datasheet",{ useNewUrlParser: true });    
+mongoose.connect("mongodb://localhost:27017/Datasheet",{ useNewUrlParser: true });    
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + '/public'));
@@ -11,7 +11,24 @@ app.get("/",function(req,res){
     var noMatch;
     if(req.query.search){
         const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-            Car.find({company:regex},function(err,foundCar){
+        // var regex = new RegExp(req.query["term"], 'i');
+        // var query = Car.find({company: regex}, { 'company': 1 }).sort({"updated_at":-1}).sort({"created_at":-1}).limit(20);
+        //     // Execute query in a callback and return users list
+        //     query.exec(function(err, cars) {
+        //         if (!err) {
+        //         // Method to construct the json result set
+        //         var result = buildResultSet(cars); 
+        //         res.send(result, {
+        //             'Content-Type': 'application/json'
+        //         }, 200);
+        //         } else {
+        //         res.send(JSON.stringify(err), {
+        //             'Content-Type': 'application/json'
+        //         }, 404);
+        //         }
+        //     });
+
+            Car.find({$or:[{company:regex}, {carName:regex},{location:regex},{insurance:regex}]},function(err,foundCar){
         if(err){
             console.log(err);
         }else{
@@ -64,6 +81,10 @@ function escapeRegex(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 };
 
-app.listen(process.env.PORT,process.env.IP,function(){
-    console.log("server is on");
-});
+
+
+app.set('port', process.env.PORT || 3000)
+app.listen(3000);
+// app.listen(process.env.PORT,process.env.IP,function(){
+//     console.log("server is on");
+// });
