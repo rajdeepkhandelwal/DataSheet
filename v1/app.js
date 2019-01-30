d972326cdefd882f4da125384d0345130881d3ec
khandelwalrajdeep <khandelwalrajdeep@outlook.com>
2019-01-30 18:38:11 +0530

    manage files

--- v1/app.js
+++ v1/app.js
@@ -7,28 +7,25 @@ mongoose.connect("mongodb://localhost:27017/Datasheet",{ useNewUrlParser: true }
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + '/public'));
-app.get("/",function(req,res){
-    var noMatch;
-    if(req.query.search){
-        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-        // var regex = new RegExp(req.query["term"], 'i');
-        // var query = Car.find({company: regex}, { 'company': 1 }).sort({"updated_at":-1}).sort({"created_at":-1}).limit(20);
-        //     // Execute query in a callback and return users list
-        //     query.exec(function(err, cars) {
-        //         if (!err) {
-        //         // Method to construct the json result set
-        //         var result = buildResultSet(cars); 
-        //         res.send(result, {
-        //             'Content-Type': 'application/json'
-        //         }, 200);
-        //         } else {
-        //         res.send(JSON.stringify(err), {
-        //             'Content-Type': 'application/json'
-        //         }, 404);
-        //         }
-        //     });
 
-            Car.find({$or:[{company:regex}, {carName:regex},{location:regex},{insurance:regex}]},function(err,foundCar){
+//====================================================
+//Routes
+//====================================================
+app.get("/",function(req,res){
+        Car.find({},function(err,foundCar){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("home",{cars:foundCar});
+        }
+    });
+    
+});
+app.get("/getdata",function(req,res){
+    var noMatch;
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+
+        Car.find({$or:[{company:regex}, {carName:regex},{location:regex},{insurance:regex}]},function(err,foundCar){
         if(err){
             console.log(err);
         }else{
@@ -36,20 +33,10 @@ app.get("/",function(req,res){
             if(foundCar.length<1){
                 noMatch="No Search Result Found";
             }
-            res.render("home",{cars:foundCar,noMatch:noMatch});
+            res.render("searchdata",{cars:foundCar,noMatch:noMatch});
         }
     });
-    }else{
-        Car.find({},function(err,foundCar){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("home",{cars:foundCar,noMatch:noMatch});
-        }
-    });
-    }
 });
-
 // app.get("/admin",function(req,res){
 //     res.render("new");
 // });
